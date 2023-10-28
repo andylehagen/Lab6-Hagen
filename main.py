1546fd06688b253fdd64202145372fac0271806e
brownzoem <brownzoem@gmail.com>
2023-10-28 15:53:32 -0400

    added decoder

--- main.py
+++ main.py
@@ -2,6 +2,12 @@
 # COP3502C
 # UFID: 82680804
 
+def decode(password):
+    decoded_password = ""
+    for i in password:
+        decoded_password += str(int(i) - 3)
+    return decoded_password
+
 
 def encode(password):
     # Create an empty string assigned to a new variable to store the encoded password
@@ -23,25 +29,26 @@ def main():
     # Menu
     print("Menu\n-------------")
     print("1. Encode\n2. Decode\n3. Quit")
-
+    encoded_password = ""
     # While loop to make a looping menu
     while True:
-        option = int(input("Please enter an option: "))
+        print("Your password has been encoded and stored!")
 
+        # Menu
+        print("Menu\n-------------")
+        print("1. Encode\n2. Decode\n3. Quit")
+        option = int(input("Please enter an option: "))
         # Encode password option
         if option == 1:
             password = input("Please enter your password to encode: ")
             encoded_password = encode(password)
-            print("Your password has been encoded and stored!")
-
-            # Menu
-            print("Menu\n-------------")
-            print("1. Encode\n2. Decode\n3. Quit")
-
         # Decode password option
         if option == 2:
             # Print decoded password here.
-            pass
+            if encoded_password == "":
+                print("No password encoded yet!")
+                #The encoded password is 45678888, and the original password is 12345555.
+            print("The encoded password is", encoded_password, ", and the original password is", decode(encoded_password))
 
         # Quit looping menu option
         if option == 3:
